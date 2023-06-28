045bfdd224dcbd2a3956165168a5b286b4efd8ad
Ni1py <gekepyrkov@gmail.com>
2023-06-28 17:19:56 +0300

    Изучил типы данных, var и val, lateinit, глубже изучил findViewById и установку слушателя нажатий

--- NewsApp/app/src/main/java/mobile/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/mobile/newsapp/MainActivity.kt
@@ -5,16 +5,44 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var a = 120 //mutable
+    val z = 100 //immutable
+
+    var b: Byte = 120
+    var s: Short = 120
+    var i: Int = 120
+    var l: Long = 120L
+
+    var f: Float = 120.8f
+    var d: Double = 120.8
+
+    var g: Boolean = true
+    var j: String = "Hello"
+    var tr = j.startsWith("hello")
+    var c: Char = 't'
+
+    //var tvTest : TextView? = null
+    lateinit var tvTest : TextView
+    lateinit var bTest : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvTest = findViewById(R.id.tvTest)
+        bTest = findViewById(R.id.bTest)
+
+        bTest.setOnClickListener {
+            tvTest.text = "Privetik"
+            var test1 = f.toString()
+        }
     }
 
-    fun onClickTest(view : View) {
-        val tvTest = findViewById<TextView>(R.id.tvTest)
-        tvTest.text = "Privetik"
-    }
+//    fun onClickStart(view : View) { //когда в параметры кидаем View, говорим системе, что это слушатель нажатий
+//        tvTest.text = "Privetik"
+//        var test1 = f.toString()
+//    }
 }
