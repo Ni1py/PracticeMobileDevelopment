c640533a58690cd1d41dff0c5365ff8c59b81c5e
Ni1py <gekepyrkov@gmail.com>
2023-06-29 15:19:50 +0300

    Произвел рефакторинг кода, поставил private где надо, добавил пакет constants, поработал с Object, вынес переисползуемый код в приватный метод

--- NewsApp/app/src/main/java/mobile/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/mobile/newsapp/MainActivity.kt
@@ -1,18 +1,14 @@
 package mobile.newsapp
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.view.LayoutInflater
+import android.provider.ContactsContract.Contacts
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import mobile.newsapp.constants.Constants
 import mobile.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mainBinding : ActivityMainBinding
+    private lateinit var mainBinding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +16,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(mainBinding.root)
 
         mainBinding.btResult.setOnClickListener {
-            val resultValue = mainBinding.edValue.text.toString().toInt()
+            val resultValue = mainBinding.edValue.text.toString()
             mainBinding.tvResult.visibility = View.VISIBLE
-            when(resultValue) {
-                in 0..1000 -> mainBinding.tvResult.text = "Вы начинающий блогер"
-                in 1001..100000 -> mainBinding.tvResult.text = "Вы средний блогер"
-                in 100001..1000000 -> mainBinding.tvResult.text = "Вы суперзвезда!"
-                else -> mainBinding.tvResult.text = "Вы нечто!"
+
+            mainBinding.tvResult.text = when(resultValue) {
+                Constants.ENGINEER -> {
+                     isCorrectPassword(
+                        mainBinding.edPassword.text.toString(),
+                        Constants.ENGINEER_PASSWORD,
+                        Constants.ENGINEER_SALARY
+                    )
+                }
+                Constants.JANITOR -> {
+                    isCorrectPassword(
+                        mainBinding.edPassword.text.toString(),
+                        Constants.JANITOR_PASSWORD,
+                        Constants.JANITOR_SALARY
+                    )
+                }
+                Constants.DIRECTOR -> {
+                    isCorrectPassword(
+                        mainBinding.edPassword.text.toString(),
+                        Constants.DIRECTOR_PASSWORD,
+                        Constants.DIRECTOR_SALARY
+                    )
+                }
+                else -> "Нет такого работника"
             }
         }
     }
+
+    private fun isCorrectPassword(text: String, password: Int, salary: Int) =
+        if (text.isNotEmpty()) {
+            if (text.toInt() == password)
+                "Получите ваши $salary"
+            else
+                "Неверный пароль"
+        }  else
+            "Вы не ввели пароль"
 }
